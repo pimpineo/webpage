7bb618033f69f41d3ee7649d37c55246b4abb40c
pimpineo <70066034+pimpineo@users.noreply.github.com>
2020-08-22 14:08:55 -0400

    Resubiendo pagina

---
+++ app/components/btn-descargar/btn-descargar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-descargar',
+  templateUrl: './btn-descargar.component.html',
+  styleUrls: ['./btn-descargar.component.css']
+})
+export class BtnDescargarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
